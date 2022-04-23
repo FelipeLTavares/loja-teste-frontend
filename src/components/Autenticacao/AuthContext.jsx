03d442a08e46706d2f799be5fb6cbcef7db67e10
Felipe L Tavares <felipelimatavares96@gmail.com>
2022-04-22 23:09:49 -0300

    5

--- src/components/Autenticacao/AuthContext.jsx
+++ src/components/Autenticacao/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 export default function AuthProvider ({children}){
   const [logado, setLogado] = useState(false);
   const [token, setToken] = useState(sessionStorage.getItem('token') || '');
-  const URL_BACKEND = "https://proj-loja-mern.vercel.app/api";
+  const URL_BACKEND = "https://loja-teste-backend.vercel.app/api";
   /* const URL_BACKEND = 'http://localhost:5000/api' */
 
   return(
